5759fd55dd5121c1adabd2b42fdf63edccbb7ae6
Stefan Schindler <dns2utf8@estada.ch>
2017-11-25 10:51:52 +0100

    Works on Mac OS X 10.13.1

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+extern crate libc;
+
+use libc::{pthread_self, pthread_getname_np, pthread_setname_np};
+use std::ffi::{CString, CStr, OsStr};
+use std::io::{self, Error};
+
+fn main() {
+    let lalala = "la la la la la la la123456789012345678901234567890 la la la l63";
+    println!("{:?}.len() = {}", lalala, lalala.len());
+    let _original = getname().expect("getname default");
+    setname(lalala).expect("setname");
+    
+    let new = getname().expect("getname changed");
+    assert_eq!(lalala, new);
+
+unsafe {
+    libc::getchar();
+}
+}
+
+fn getname() -> std::io::Result<String> {
+    unsafe {
+        let my_id = pthread_self();
+        let mut name = [0i8; 256];
+        let ret = pthread_getname_np(my_id, name.as_mut_ptr(), name.len());
+
+        let name = CStr::from_ptr(name.as_ptr())
+                    .to_string_lossy()
+                    .to_owned().into_owned();
+
+        println!("{} => {:?}", ret, name);
+
+        if ret == 0 {
+            Ok(name)
+        } else {
+            Err(Error::last_os_error())
+        }
+    }
+}
+
+fn setname(new: &str) -> io::Result<()> {
+    unsafe {
+        let ret = pthread_setname_np(new.as_ptr() as *const i8);
+        if ret == 0 {
+            Ok( () )
+        } else {
+            Err(Error::last_os_error())
+        }
+    }
+}
